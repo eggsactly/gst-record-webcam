84a79083d460b713bfe0b6dc7904f8090d401a69
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-01 12:19:10 -0700

    Added setting audio buffer size with AUDIO_QUEUE_SECONDS variable.

--- record.sh
+++ record.sh
@@ -27,18 +27,29 @@ SECOND_CAM_VIDEO=$(v4l2-ctl --list-devices | ./get-video-device.py $(pacmd list-
 
 OUTPUT_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
-echo "Main cam video $MAIN_CAM_VIDEO"
-echo "Main cam audio $MAIN_CAM_AUDIO"
+AUDIO_QUEUE_SECONDS=1
+AUDIO_RECORD_RATE=44100
 
-echo "Second cam video $SECOND_CAM_VIDEO"
-echo "Second cam audio $SECOND_CAM_AUDIO"
+AUDIO_QUEUE_NS=$(expr 1000000000 \* $AUDIO_QUEUE_SECONDS)
+AUDIO_BUFFERS=$(expr $AUDIO_RECORD_RATE \* $AUDIO_QUEUE_SECONDS)
 
-echo "Filename $OUTPUT_FILE_NAME"
+echo "Main cam video:   $MAIN_CAM_VIDEO"
+echo "Main cam audio:   $MAIN_CAM_AUDIO"
+
+echo "Second cam video: $SECOND_CAM_VIDEO"
+echo "Second cam audio: $SECOND_CAM_AUDIO"
+
+echo "Filename:         $OUTPUT_FILE_NAME"
+
+echo "AUDIO_QUEUE_NS:   $AUDIO_QUEUE_NS"
+echo "AUDIO_BUFFERS:    $AUDIO_BUFFERS"
 
 # Record video and audio from the main webcam 
 gst-launch-1.0 -v v4l2src device=$MAIN_CAM_VIDEO ! video/x-h264,width=1920,height=1080,framerate=30/1 ! tee name=t \
 	t. ! queue ! vaapih264dec ! queue ! videoconvert ! videoscale ! video/x-raw,height=480 ! timeoverlay ! queue ! autovideosink \
 	t. ! queue ! h264parse ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=1000000000 ! mux. \
-	pulsesrc device=$MAIN_CAM_AUDIO ! audioconvert ! queue ! avenc_aac ! mux. \
-	mpegtsmux name=mux ! queue ! filesink location=$OUTPUT_FILE_NAME
+	pulsesrc device=$MAIN_CAM_AUDIO ! audioconvert ! audio/x-raw, rate=$AUDIO_RECORD_RATE ! queue max-size-buffers=$AUDIO_BUFFERS max-size-time=$AUDIO_QUEUE_NS ! avenc_aac ! queue ! mux. \
+	mpegtsmux name=mux ! queue max-size-time=1000000000 ! filesink location=$OUTPUT_FILE_NAME
+
+
 
