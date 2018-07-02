aa66dfc4e60d5d6632da4f7c2592578375f7044f
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-01 17:05:37 -0700

    Got video from two webcams on one screen with audio from one.

--- record.sh
+++ record.sh
@@ -45,11 +45,12 @@ echo "Filename:         $OUTPUT_FILE_NAME"
 echo "AUDIO_QUEUE_NS:   $AUDIO_QUEUE_NS"
 
 # Record video and audio from the main webcam 
-gst-launch-1.0 -v v4l2src device=$MAIN_CAM_VIDEO ! video/x-raw,width=1920,height=1080,framerate=30/1 ! tee name=t \
-	t. ! queue ! videoconvert ! videoscale ! video/x-raw,height=480 ! timeoverlay ! queue leaky=upstream ! autovideosink sync=false \
+gst-launch-1.0 -v \
+    v4l2src device=$MAIN_CAM_VIDEO ! video/x-raw,width=1920,height=1080,framerate=30/1 ! queue ! mix. \
+    v4l2src device=$SECOND_CAM_VIDEO ! video/x-raw, width=640,height=480,framerate=30/1 ! queue ! mix. \
+    videomixer name=mix sink_1::xpos=1280 sink_1::ypos=600 sink_1::zorder=2 ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=1000000000 ! queue ! videoconvert ! video/x-raw,width=1920,height=1080 ! tee name=t \
+	t. ! queue ! videoscale ! video/x-raw,height=480 ! timeoverlay ! queue leaky=upstream ! autovideosink sync=false \
 	t. ! queue ! vaapih264enc ! h264parse ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=1000000000 ! mux. \
 	pulsesrc device=$MAIN_CAM_AUDIO ! audioconvert ! audio/x-raw, rate=$AUDIO_RECORD_RATE ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=$AUDIO_QUEUE_NS ! avenc_aac ! aacparse ! queue ! mux. \
 	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
-
-
 
